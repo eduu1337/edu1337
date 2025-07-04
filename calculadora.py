3cb3c6e2005413f6465517ceafeb11c44a240034
edu1337 <eduardo.martin@escola.pr.gov.br>
2025-07-04 08:48:02 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,12 @@
+#######################
+# Calculadora Simples
+#Edu N°8  1°E
+#######################
 
+print("Vamos somar dois números!")
+num1 = float(input("Digite o seu primeiro número: "))
+num2 = float(input("Digite o seu segundo número: "))
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é {soma}.")
+
+#######################
