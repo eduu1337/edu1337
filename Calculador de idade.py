c6bc15e27ae2f0d7e984b8c53bbfcf1cb66e9e5a
edu1337 <eduardo.martin@escola.pr.gov.br>
2025-07-04 09:36:25 -0300

    Add files via upload

---
+++ Calculador de idade.py
@@ -0,0 +1,14 @@
+#######################
+# Calculadora de Idade
+#Edu N°8  1°E
+#######################
+
+print("Vamos calcular sua idade!")
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
+ano_atual = int(input("Digite o ano atual: "))
+
+idade = ano_atual - ano_nascimento
+
+print(f"Sua idade é aproximadamente: {idade} anos.")
+
+#######################
