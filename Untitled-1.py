71606ac9c314127953116934de611c4a3fdabc11
eduu1337 <eduardo.martin@escola.pr.gov.br>
2025-08-18 10:27:54 -0300

    CNPJ_EDUARDO

---
+++ Untitled-1.py
@@ -0,0 +1,25 @@
+############################
+# Eduardo Alves Martin N°8
+# 1°E 18/08/2025
+############################
+TAM_CNPJ = 18
+cnpj = input("Por favor, insira o CNPJ:")
+flag = True
+
+if len(cnpj) != TAM_CNPJ:
+    flag = False
+elif (cnpj[2] != "." or cnpj[6] != "." or cnpj[10] != "/" or cnpj[15] != "-"):
+    flag = False
+else:
+    for i in range(TAM_CNPJ):
+        if (i != 2 and i != 6 and i != 10 and i != 15):
+            c = cnpj[i]
+            if not c.isdigit():
+                flag = False
+                break
+
+if flag:
+    print("Formato de CNPJ Válido.")
+else:
+    print("O CNPJ informado não tem um formato válido.")
+############################
